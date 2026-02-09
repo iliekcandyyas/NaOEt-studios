97ad1f9eae119d075d5ce253fda7a4169d41b3b4
iliekcandyyas <ilanthedude@gmail.com>
2026-02-09 15:38:05 +0500

    Add fade-in effect using IntersectionObserver

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const btn = document.getElementById("contactBtn");
 btn.addEventListener("click", () => {
   alert("Email us at ---");
 });
+
+const faders = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+faders.forEach(el => observer.observe(el));
+
