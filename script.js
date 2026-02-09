d22a2107ea6ca71ead306b97072d0fe60936f82c
iliekcandyyas <ilanthedude@gmail.com>
2026-02-09 16:00:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const btn = document.getElementById("contactBtn");
-
-btn.addEventListener("click", () => {
-  alert("Email us at ---");
-});
-
 const faders = document.querySelectorAll(".fade");
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2
+  }
+);
 
 faders.forEach(el => observer.observe(el));
 
+
+
+window.dispatchEvent(new Event("scroll"));
