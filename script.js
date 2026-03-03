95a8b09bf96c18d1878205b74ad1a74cb21c4b10
iliekcandyyas <ilanthedude@gmail.com>
2026-03-04 03:41:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,35 @@ function reveal() {
   });
 }
 
+const canvas = document.getElementById("bg");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars = [];
+
+for (let i = 0; i < 100; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    radius: ಪುತ್ರMath.random() * 2
+  });
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  stars.forEach(star => {
+    ctx.beginPath();
+    ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
+    ctx.fill();
+  });
+  requestAnimationFrame(draw);
+}
+
+draw();
+
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
 
